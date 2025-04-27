07d1d20414223d9bd8c23adcb83ac167dc484436
OathInMoonlight <OathInMoonlight@foxmail.com>
2025-04-28 02:34:47 +0900

    表头按钮

--- BangumiList-Frontend/src/components/TableComponent.vue
+++ BangumiList-Frontend/src/components/TableComponent.vue
@@ -4,13 +4,15 @@
         <v-skeleton-loader type="table-tbody" />
     </div>
     <div id="fillHeightDiv" v-if="!loading">
-        <v-table fixed-header class="fill-height">
+        <v-table fixed-header hover="true" class="fill-height">
             <thead>
                 <label v-if="tableHeaders.length == 0" class="d-flex justify-center align-center ma-8">{{
                     $lang.text.noData[$lang.currentLang] }}</label>
                 <tr>
-                    <th v-for="header in tableHeaders" :width="header.width" class="border-e">
-                        <label :class="'d-flex justify-' + header.align">{{ header.text }}</label>
+                    <th v-for="header in tableHeaders" :width="header.width" class="border-e pa-0 fill-height">
+                        <v-btn block variant="text" rounded="0" size="x-large">
+                            <label class="text-body-1">{{ header.text }}</label>
+                        </v-btn>
                     </th>
                 </tr>
             </thead>
@@ -18,7 +20,7 @@
                 <tr v-for="row in tableData">
                     <td v-for="header in tableHeaders" class="border-e">
                         <label id="tableItemText" :class="'d-flex justify-' + header.align">{{ row[header.value]
-                            }}</label>
+                        }}</label>
                     </td>
                 </tr>
             </tbody>
